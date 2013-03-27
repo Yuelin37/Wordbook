41d43a78a66ad76b5638640be0e650afabcae8ae
Yuelin37 <yuelinyan@gmail.com>
2013-03-27 21:35:14 +0800

    Refactor and add comments

--- Wordbook/src/main/Wordbook.java
+++ Wordbook/src/main/Wordbook.java
@@ -9,29 +9,27 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class Wordbook {
 	public static void main(String[] args) throws IOException {
-		// Create a new instance of the Firefox driver
-		// Notice that the remainder of the code relies on the interface,
-		// not the implementation.
+		// Create a new instance of the Chrome driver
+		// For Mac and Windows, use different binary
 		String currentOS = System.getProperty("os.name");
-		if (currentOS.contains("Mac")){
-			System.setProperty("webdriver.chrome.driver",
-				"driver/chromedriver");
+		if (currentOS.contains("Mac")) {
+			System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
 		}
-		
-		if (currentOS.contains("Windows")){
+		if (currentOS.contains("Windows")) {
 			System.setProperty("webdriver.chrome.driver",
-				"driver/chromedriver.exe");
+					"driver/chromedriver.exe");
 		}
-		
 		WebDriver driver = new ChromeDriver();
 
-		// And now use this to visit Google
+		// And now use this to visit Youdo dictionary query page   
 		driver.get("http://dict.youdao.com/search?le=eng&q=flint&keyfrom=dict.index");
 		// Alternatively the same thing can be done like this
 		// driver.navigate().to("http://www.google.com");
-
+		
+		// Login
 		WebElement login = driver.findElement(By.linkText("登录"));
 		login.click();
 		WebElement username = driver.findElement(By.id("username"));
@@ -40,76 +38,53 @@ public class Wordbook {
 		pword.sendKeys("hanxiaomei");
 		pword.submit();
 
-		// Find the text input element by its name
-		// WebElement element = driver.findElement(By.name("q"));
-
-		// Enter something to search for
-		// element.sendKeys("flint");
-
-		// Now submit the form. WebDriver will find the form for us from the
-		// element
-		// element.submit();
-
-		// Check the title of the page
-		// System.out.println("Page title is: " + driver.getTitle());
-
-		// Google's search is rendered dynamically with JavaScript.
-		// Wait for the page to load, timeout after 10 seconds
-		/*
-		 * (new WebDriverWait(driver, 10)).until(new
-		 * ExpectedCondition<Boolean>() { public Boolean apply(WebDriver d) {
-		 * return d.getTitle().toLowerCase().startsWith("cheese!"); } });
-		 */
-
-		// Should see: "cheese! - Google Search"
-		// System.out.println("Page title is: " + driver.getTitle());
-
-		// Close the browser
-
-		// (new WebDriverWait(driver, 10)).until(new
-		// ExpectedCondition<Boolean>() {
-		// public Boolean apply(WebDriver d) {
-		// return d.getTitle().toLowerCase().contains("flint");
-		// }
-		// });
-
-		// Find the text input element by its name
 		
+
+		// Find the 'Add To Wordbook' button
 		WebElement addButton = driver.findElement(By.id("wordbook"));
+		
 		try {
-			// 1. Buffered input file
-			DataInputStream in = new DataInputStream(new BufferedInputStream(
-					new FileInputStream("output.txt")));
-			in.readLine();
-			String s = new String();
+			// Open the file which contains the words you want to add
+			FileInputStream fstream = new FileInputStream("output.txt");
+			// Get the object of DataInputStream
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String newWord;
+			// TODO
+			// I have some problem with the first chars in the first line
+			// Read the first line, so the word in the first line will not be prcossed
+			newWord = br.readLine();
 			
-			int i = 0;
-			while ((s = in.readLine()) != null && i <400){
-				i ++;
-			}
-			
-			while ((s = in.readLine()) != null){				
-				if (s.length() > 0 && s.length()<=20 && !s.contains(" ")){
-					System.out.println("http://dict.youdao.com/search?le=eng&q="+s+"&keyfrom=dict.index");
-					driver.get("http://dict.youdao.com/search?le=eng&q="+s+"&keyfrom=dict.index");
+			// Read File Line By Line, start from the second line
+			while ((newWord = br.readLine()) != null) {
+				// Print the content on the console
+				System.out.println(newWord);
+				if (newWord.length() > 0 && newWord.length() <= 20
+						&& !newWord.contains(" ")) {
+					System.out
+							.println("http://dict.youdao.com/search?le=eng&q="
+									+ newWord + "&keyfrom=dict.index");
+					driver.get("http://dict.youdao.com/search?le=eng&q="
+							+ newWord + "&keyfrom=dict.index");
 					addButton = driver.findElement(By.id("wordbook"));
 					if (addButton.getAttribute("class").contains("add"))
 						addButton.click();
-					System.out.println(addButton.getAttribute("class").toString());
+					System.out.println(addButton.getAttribute("class")
+							.toString());
 					try {
-						Thread.sleep(5000);
+						Thread.sleep(3000);
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 				}
-					
 			}
-			in.close();
 
-		} catch (EOFException e) {
-			System.out.println("End of stream encountered");
+			// Close the input stream
+			in.close();
+		} catch (Exception e) {// Catch exception if any
+			System.err.println("Error: " + e.getMessage());
 		}
-		
+
 	}
 }
