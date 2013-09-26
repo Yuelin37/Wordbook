b712ddc0d328d30f7c5856ec63a4a84ba53bdcda
Yuelin37 <yuelinyan@gmail.com>
2013-09-26 11:01:33 +0800

    ParseFile.java to process the new words file from Hanvon.

--- Wordbook/src/main/ParseFile.java
+++ Wordbook/src/main/ParseFile.java
@@ -1,31 +1,46 @@
 package main;
 
 import java.io.*;
+import java.util.ArrayList;
 
-public class ReadFileTest {
+public class ParseFile {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
+		ArrayList newWordList = new ArrayList();
+
 		try {
 			// Open the file that is the first
 			// command line parameter
-			FileInputStream fstream = new FileInputStream("output.txt");
+			FileInputStream fstream = new FileInputStream("newword.txt");
 			// Get the object of DataInputStream
 			DataInputStream in = new DataInputStream(fstream);
 			BufferedReader br = new BufferedReader(new InputStreamReader(in));
 			String strLine;
 			// Read File Line By Line
 			while ((strLine = br.readLine()) != null) {
-				// Print the content on the console
-				System.out.println(strLine);
+				if (strLine.indexOf("/") < 0 && strLine.indexOf("[")<0 && strLine.length()>1) {
+					newWordList.add(strLine);
+				}
 			}
 			// Close the input stream
 			in.close();
+
+			FileOutputStream out = new FileOutputStream("newoutput.txt");
+			for (int i = 0; i < newWordList.size(); i++) {
+				out.write(newWordList.get(i).toString().getBytes());
+				out.write("\r\n".getBytes());
+			}
+
+			// Close the input stream
+			out.close();
 		} catch (Exception e) {// Catch exception if any
 			System.err.println("Error: " + e.getMessage());
 		}
+
 	}
 
 }
